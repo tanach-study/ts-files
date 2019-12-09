02d87ffdeb57fa27d7b93fc4f94bd3e1b3ce9fb3
Joey Pinhas <jpinhas97@gmail.com>
2019-12-09 01:35:52 -0500

    Add aws region

--- tssite/__init__.py
+++ tssite/__init__.py
@@ -4,4 +4,5 @@ from django.conf import settings
 client = boto3.client(
     'elastictranscoder',
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    region_name=settings.AWS_REGION)
