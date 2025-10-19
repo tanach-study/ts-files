429d8bfde6af0e9210c3770badc51c7d0dfe5bfa
Joey Pinhas <jpinhas97@gmail.com>
2025-10-19 14:59:17 -0400

    ruff check && ruff format

--- tssite/__init__.py
+++ tssite/__init__.py
@@ -1,9 +1,10 @@
 import os
+
 import boto3
 from django.conf import settings
 
 # Only initialize boto3 client if not in CI environment
-if not os.getenv('CI'):
+if not os.getenv("CI"):
     client = boto3.client(
         "elastictranscoder",
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
