7d78ef303038ad6575b88ffb46ea02fb9e93564e
Joey Pinhas <jpinhas97@gmail.com>
2025-10-19 14:56:01 -0400

    Do not init boto3 client in CI

--- tssite/__init__.py
+++ tssite/__init__.py
@@ -1,9 +1,14 @@
+import os
 import boto3
 from django.conf import settings
 
-client = boto3.client(
-    "elastictranscoder",
-    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_REGION,
-)
+# Only initialize boto3 client if not in CI environment
+if not os.getenv('CI'):
+    client = boto3.client(
+        "elastictranscoder",
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_REGION,
+    )
+else:
+    client = None
